6f8bf20a8589544160bbf593b78d1bd9fc40febf
Daniel <daniel.thavares@gmail.com>
2023-02-10 16:44:30 -0300

    ajustes na configuração de injeção de dependencias

--- src/infra/inversify.config.ts
+++ src/infra/inversify.config.ts
@@ -1,24 +1,8 @@
 import { Container } from "inversify";
-import { TYPES } from "./types";
-import { INotaServicoRepository } from "../core/interfaces/repositories/nota-servico.irepository";
-import { INovaNotaServicoUseCase } from "../core/interfaces/usecases/nova-nota-servico.interface";
-import { NovaNotaServicoUseCase } from "../core/usecases/nova-nota-servico";
-import { INotaServicoService } from "../core/interfaces/services/nota-servico.interface";
-import { NotaServicoService } from "../core/service/nota-servico.service";
-import { NotaServicoMemoryRepository } from "./repositories/nota-servico-memory.repository";
+import { useNotaServico } from "./nota-servico.config";
 
 const container = new Container();
 
-container
-  .bind<INotaServicoRepository>(TYPES.INotaServicoRepository)
-  .to(NotaServicoMemoryRepository);
-
-container
-  .bind<INovaNotaServicoUseCase>(TYPES.INovaNotaServicoUseCase)
-  .to(NovaNotaServicoUseCase);
-
-container
-  .bind<INotaServicoService>(TYPES.INotaServicoService)
-  .to(NotaServicoService);
+useNotaServico(container);
 
 export { container };
